df645dbf7472ce87a110ff39d059e696992c85cc
Christoph Rosemann <christoph.rosemann@desy.de>
2014-05-21 15:01:39 +0000

    added the other intersection calculations with tests

--- helpers/src/intersections.cc
+++ helpers/src/intersections.cc
@@ -5,8 +5,21 @@ namespace aidaTT
 {
     intersections intersectCircleCircle(const circle& circ1, const circle& circ2)
     {
-        return intersections();
+        double x1 = circ1.center().first;
+        double y1 = circ1.center().second;
+        double r1 = circ1.radius();
 
+        double x2 = circ2.center().first;
+        double y2 = circ2.center().second;
+        double r2 = circ2.radius();
+
+        /// reduce the two circles to a straight line
+        double nx = 2 * (x2 - x1);
+        double ny = 2 * (y2 - y1);
+        double dist = r1 * r1 - r2 * r2 - x1 * x1 + x2 * x2 - y1 * y1 + y2 * y2;
+
+        straightLine reducedCircle(nx, ny, dist);
+        return intersectCircleStraightLine(circ1, reducedCircle);
     }
 
 
@@ -20,9 +33,9 @@ namespace aidaTT
         sL.move(x0, y0);
 
         // check if it does not intersect or just touch
-        const double discriminant = c.r2() - sL.d2();
+        const double discriminant = c.r2() * sL.normalSquare() - sL.d2();
 
-        if(discriminant  < 0. &&  fabs(discriminant) > 1e-6)
+        if(discriminant  < 0. &&  fabs(discriminant) > 1e-9)
             return intersections();
 
         const double nx = sL.normal().first;
@@ -30,17 +43,17 @@ namespace aidaTT
         const double d = sL.distance();
         const double DISC = sqrt(discriminant);
 
-        const double x1 = x0  + (nx * d + ny * DISC);
-        const double y1 = y0  + (ny * d - nx * DISC);
+        const double x1 = x0  + (nx * d + ny * DISC) / sL.normalSquare();
+        const double y1 = y0  + (ny * d - nx * DISC) / sL.normalSquare();
 
         intersections retType;
         retType.add(x1, y1);
 
         /// return only a single solution, if both are very close
-        if(fabs(discriminant) < 1e-9)
+        if(fabs(discriminant) <= 1e-9)
             return retType;
-        const double x2 = x0  + (nx * d - ny * DISC);
-        const double y2 = y0  + (ny * d + nx * DISC);
+        const double x2 = x0  + (nx * d - ny * DISC) / sL.normalSquare();
+        const double y2 = y0  + (ny * d + nx * DISC) / sL.normalSquare();
         retType.add(x2, y2);
         return retType;
     }
@@ -49,6 +62,25 @@ namespace aidaTT
 
     intersections intersectStraightLineStraightLine(const straightLine& line1, const straightLine& line2)
     {
-        return intersections();
+        const double nx1 = line1.normal().first;
+        const double ny1 = line1.normal().second;
+        const double d1 = line1.distance();
+
+        const double nx2 = line2.normal().first;
+        const double ny2 = line2.normal().second;
+        const double d2 = line2.distance();
+
+        intersections retType;
+
+        /// test linear independency
+        const double discriminant = nx1 * ny2 - nx2 * ny1;
+        if(discriminant < 1e-9)
+            return retType;
+
+        const double x = (d1 * ny2 - ny1 * d2) / discriminant;
+        const double y = (d2 * nx1 - d1 * nx2) / discriminant;
+        retType.add(x, y);
+
+        return retType;
     }
 }
--- test/unitTests/intersectionTest.cc
+++ test/unitTests/intersectionTest.cc
@@ -6,21 +6,37 @@ using namespace aidaTT;
 intersectionTest::intersectionTest() : UnitTest("IntersectionCalculationTest", __FILE__)
 {
     c1 = new circle(0., 0., 1.);
+    c2 = new circle(1., 1., 1.);
     sL1 = new straightLine(0., 1., .5);
+    sL2 = new straightLine(1., 1., 0.);
 }
 
 
 
 void intersectionTest::_test()
 {
+    /// check circle and circle
+    intersections sectCC = intersectCircleCircle(*c1, *c2);
+    test_(sectCC.number() == 2);
+    test_(floatCompare(sectCC[0].first, 1.));
+    test_(floatCompare(sectCC[0].second, 0.));
+    test_(floatCompare(sectCC[1].first, 0.));
+    test_(floatCompare(sectCC[1].second, 1.));
 
-    intersections sect = intersectCircleStraightLine(*c1, *sL1);
-    test_(sect.number() == 2);
-    test_(floatCompare(sect[0].first, sqrt(.75)));
-    test_(floatCompare(sect[0].second, 0.5));
-    test_(floatCompare(sect[1].first, -sqrt(.75)));
-    test_(floatCompare(sect[1].second, 0.5));
-//    test_(floatCompare(_one->getYPerp(),     _one->getTrackParameters()(4)));
+    /// check circle and line
+    intersections sectCL = intersectCircleStraightLine(*c1, *sL1);
+    test_(sectCL.number() == 2);
+    test_(floatCompare(sectCL[0].first, sqrt(.75)));
+    test_(floatCompare(sectCL[0].second, 0.5));
+    test_(floatCompare(sectCL[1].first, -sqrt(.75)));
+    test_(floatCompare(sectCL[1].second, 0.5));
+
+    /// check line and line
+    intersections sectLL = intersectStraightLineStraightLine(*sL2, *sL1);
+    test_(sectLL.number() == 1);
+    test_(floatCompare(sectLL[0].first, -0.5));
+    test_(floatCompare(sectLL[0].second, 0.5));
+    //    test_(floatCompare(_one->getYPerp(),     _one->getTrackParameters()(4)));
 
 }
 
