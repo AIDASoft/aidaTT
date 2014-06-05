b6401dcc4fd889c55b85805b3ec6e0a57e05555a
Christoph Rosemann <christoph.rosemann@desy.de>
2014-06-05 15:49:17 +0000

    update unit tests: vector and matrix operations work M * M, M * V ! only one initialization method doesn't work

--- test/unitTests/matrixOps.cc
+++ test/unitTests/matrixOps.cc
@@ -13,6 +13,16 @@ matrixOps::matrixOps() : UnitTest("MatrixOperations", __FILE__)
     _matrix1 = new fiveByFiveMatrix();
     _matrix2 = new fiveByFiveMatrix(_values);
 
+    double vv[] = { 0.1, 0.2, 0.3, 0.4, 0.5 };
+    _vvv.resize(5);
+    _vvv.assign(vv, vv + 5);
+
+    _vec1 = new Vector5(1.1, 2.2, 3.3, 4.4, 5.5);
+    _vec2 = new Vector5();
+    _vec3 = new Vector5(*_vec1);
+    //_vec4 = new Vector5(vv);
+    _vec5 = new Vector5(_vvv);
+
 }
 
 
@@ -52,7 +62,53 @@ void matrixOps::_test()
                 }
         }
 
+    fiveByFiveMatrix productMM = (*_matrix2)  * (*_matrix2);
 
+    test_(floatCompare(productMM(0, 0) , 215));
+    test_(floatCompare(productMM(0, 1) , 230));
+    test_(floatCompare(productMM(0, 2) , 245));
+    test_(floatCompare(productMM(0, 3) , 260));
+    test_(floatCompare(productMM(0, 4) , 275));
+
+    test_(floatCompare(productMM(1, 0) , 490));
+    test_(floatCompare(productMM(1, 1) , 530));
+    test_(floatCompare(productMM(1, 2) , 570));
+    test_(floatCompare(productMM(1, 3) , 610));
+    test_(floatCompare(productMM(1, 4) , 650));
+
+    test_(floatCompare(productMM(2, 0) , 765));
+    test_(floatCompare(productMM(2, 1) , 830));
+    test_(floatCompare(productMM(2, 2) , 895));
+    test_(floatCompare(productMM(2, 3) , 960));
+    test_(floatCompare(productMM(2, 4) , 1025));
+
+    test_(floatCompare(productMM(3, 0) , 1040));
+    test_(floatCompare(productMM(3, 1) , 1130));
+    test_(floatCompare(productMM(3, 2) , 1220));
+    test_(floatCompare(productMM(3, 3) , 1310));
+    test_(floatCompare(productMM(3, 4) , 1400));
+
+    test_(floatCompare(productMM(4, 0) , 1315));
+    test_(floatCompare(productMM(4, 1) , 1430));
+    test_(floatCompare(productMM(4, 2) , 1545));
+    test_(floatCompare(productMM(4, 3) , 1660));
+    test_(floatCompare(productMM(4, 4) , 1775));
+
+    Vector5 productMV = (* _matrix2)  * (*_vec1);
+
+    test_(floatCompare(productMV(0) , 60.5));
+    test_(floatCompare(productMV(1) , 143.));
+    test_(floatCompare(productMV(2) , 225.5));
+    test_(floatCompare(productMV(3) , 308.));
+    test_(floatCompare(productMV(4) , 390.5));
+
+    for(unsigned int j = 0; j < 5; ++j)
+        {
+            test_(floatCompare((*_vec2)(j), 0.));
+            test_(floatCompare((*_vec1)(j), (*_vec3)(j)));
+            test_(floatCompare((*_vec5)(j), _vvv[j]));
+            // test_( floatCompare ( (*_vec4)(j), (*_vec5)(j) ) );
+        }
 }
 
 
--- test/unitTests/matrixOps.hh
+++ test/unitTests/matrixOps.hh
@@ -1,8 +1,9 @@
 #ifndef MATRIXOPS_HH
 #define MATRIXOPS_HH
 
-/// initialization, access, write tests for matrix abstraction class
+/// initialization, access, write tests for matrix and vector abstraction class
 #include "fiveByFiveMatrix.hh"
+#include "Vector5.hh"
 
 #include "UnitTest.hh"
 #include <vector>
@@ -21,7 +22,13 @@ class matrixOps : public UnitTesting::UnitTest
         aidaTT::fiveByFiveMatrix* _matrix1;
         aidaTT::fiveByFiveMatrix* _matrix2;
         aidaTT::fiveByFiveMatrix* _matrix3;
+        aidaTT::Vector5* _vec1;
+        aidaTT::Vector5* _vec2;
+        aidaTT::Vector5* _vec3;
+        aidaTT::Vector5* _vec4;
+        aidaTT::Vector5* _vec5;
         std::vector<double> _values;
+        std::vector<double> _vvv;
 
 };
 #endif // MATRIXOPS_HH
