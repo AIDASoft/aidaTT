6fb38bf4cec3f52f9ab0fdaa402c4778d15caead
Frank Gaede <frank.gaede@desy.de>
2015-11-26 19:15:51 +0000

    - added printout of surfaces and integrated X0

--- examples/material_effects/check_materials.cpp
+++ examples/material_effects/check_materials.cpp
@@ -22,8 +22,6 @@ using namespace std ;
  * along a straight line.
  * 
  */
-
-
 int main(int argc, char** argv) {
   
   if(argc < 3)
@@ -49,12 +47,9 @@ int main(int argc, char** argv) {
 
   aidaTT::Vector3D dirVec( 1. , phi , theta,  aidaTT::Vector3D::spherical ) ;
 
-  std::cout << " --- will check materials along : " << dirVec << std::endl ;
-
-
   // create an (almost) straight track in that direction
   double omega = 1.e-12 ; 
-  double tanl  = M_PI - theta ;
+  double tanl  = tan( 0.5*M_PI - theta ) ;
   double phi0  = phi ;
   double d0    = 0. ;
   double z0    = 0. ;
@@ -62,6 +57,11 @@ int main(int argc, char** argv) {
 
   aidaTT::trackParameters tP( aidaTT::Vector5(omega, tanl, phi0 , d0 , z0 ), aidaTT::Vector3D() )  ;
   
+
+  std::cout << " --- will check materials along : " << dirVec << std::endl ;
+  
+  std::cout << " --- track parameters : " << tP << std::endl ;
+
   const aidaTT::IGeometry& geom = aidaTT::IGeometry::instance( inFile ) ;
 
   const aidaTT::SurfaceVec& surfaces = geom.getSurfaces() ;
@@ -72,11 +72,60 @@ int main(int argc, char** argv) {
 
   aidaTT::IntersectionVec intersections = traj.getIntersectionsWithSurfaces( surfaces ) ;
 
+  double X0_tot = 0. ;
 
-  for(unsigned i=0,N=intersections.size() ; i<N ; ++i){
+ for(unsigned i=0,N=intersections.size() ; i<N ; ++i){
+    
+    
+    const aidaTT::ISurface* surf = intersections[i].second ;
 
-    std::cout << " found intersection at s = " << intersections[i].first << " : "
-	      <<  *intersections[i].second << std::endl ;
+    double s =  0;
+    aidaTT::Vector3D xx ;
+    bool intersects = aidaTT::intersectWithSurface(  surf , 
+						     tP.parameters() , tP.referencePoint() ,
+						     s, xx, 0 , true ) ; 
+
+    //----------------- get X0 --------------------------------------------
+    const DDSurfaces::IMaterial& material_inn = surf->innerMaterial();
+    const DDSurfaces::IMaterial& material_out = surf->outerMaterial();
+    
+    const double r_i = surf->innerThickness();
+    const double r_o = surf->outerThickness();
+    
+    const double X0_o = material_out.radiationLength();
+    const double X0_i = material_inn.radiationLength();
+    
+    double r_tot = r_i + r_o ;
+    
+    //calculation of effective radiation length of the surface
+    double X0_eff = ( r_i/X0_i + r_o/X0_o ) / r_tot ; 
+    
+    //calculation of the path of the particle inside the material
+    //compute path as projection of (straight) track to surface normal:
+    
+    const aidaTT::Vector3D& up = dirVec.unit() ;
+
+        // need to get the normal at crossing point 
+    const aidaTT::Vector3D& n = surf->normal( xx ) ;
+    
+    double cosTrk = std::fabs( up * n )  ;
+    
+    double path = r_i + r_o ;
+    
+    path = path/cosTrk ; 
+    
+    double X_X0 = path * X0_eff ;
+
+    //--------------------------------------------------------------------
+
+    X0_tot += X_X0 ;
+
+
+    std::cout << " found intersection at s = " << intersections[i].first << ", " 
+	      << xx << " surf: " << std::endl 
+	      << *surf 
+	      << " X0 : " << X_X0   << "   - total : " << X0_tot << std::endl ;;
+
 
   }
 
