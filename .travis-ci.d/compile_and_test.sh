6a8dfc534e3006d4338c94c8ea879ecf2c2928af
Marko Petric <marko.petric@cern.ch>
2016-12-08 10:10:37 +0100

    Add LLVM CI

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-
-ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/current/CI_gcc
+ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/current/CI_${COMPILER}
 source $ILCSOFT/init_ilcsoft.sh
 
-cd /aidaTT
+cd /Package
 mkdir build
 cd build
 cmake -GNinja -C $ILCSOFT/ILCSoft.cmake ..
 ninja
-
+ctest --output-on-failure
